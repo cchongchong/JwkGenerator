a37ecef5b8c6800f94002b0657709c475d7695b7
Chongzhao Wang <cchongchong@users.noreply.github.com>
2020-02-12 14:49:32 -0500

    Initial Commit

---
+++ JwkGenerator/JsonWebKeyDto.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace JwkGenerator
+{
+    public class JsonWebKeyDto
+    {
+        public JsonWebKeyDto(X509Certificate2 certificate)
+        {
+            Kid = certificate.Thumbprint;
+            X5t = Base64UrlEncoder.Encode(certificate.GetCertHash());
+            X5c = new[] { Convert.ToBase64String(certificate.RawData) };
+            Use = "sig";
+
+            if ((certificate.PrivateKey ?? certificate.PublicKey.Key) is RSA rsa)
+            {
+                Kty = JsonWebAlgorithmsKeyTypes.RSA;
+
+                var parameters = rsa.ExportParameters(certificate.HasPrivateKey);
+
+                E = Base64UrlEncoder.Encode(parameters.Exponent);
+                N = Base64UrlEncoder.Encode(parameters.Modulus);
+                if (certificate.HasPrivateKey)
+                {
+                    D = Base64UrlEncoder.Encode(parameters.D);
+                    DP = Base64UrlEncoder.Encode(parameters.DP);
+                    DQ = Base64UrlEncoder.Encode(parameters.DQ);
+                    P = Base64UrlEncoder.Encode(parameters.P);
+                    Q = Base64UrlEncoder.Encode(parameters.Q);
+                    QI = Base64UrlEncoder.Encode(parameters.InverseQ);
+                }
+            }
+            else if ((certificate.PrivateKey ?? certificate.PublicKey.Key) is ECDsa ecdsa)
+            {
+                Kty = JsonWebAlgorithmsKeyTypes.EllipticCurve;
+
+                var parameters = ecdsa.ExportParameters(certificate.HasPrivateKey);
+                X = Base64UrlEncoder.Encode(parameters.Q.X);
+                Y = Base64UrlEncoder.Encode(parameters.Q.Y);
+                Crv = parameters.Curve.Oid.Value switch
+                {
+                    "1.2.840.10045.3.1.7" => JsonWebKeyECTypes.P256,
+                    "1.3.132.0.34" => JsonWebKeyECTypes.P384,
+                    "1.3.132.0.35" => JsonWebKeyECTypes.P521,
+                    _ => throw new InvalidOperationException($"Unsupported curve type of {parameters.Curve.Oid.Value} - {parameters.Curve.Oid.FriendlyName}"),
+                };
+                if (certificate.HasPrivateKey)
+                {
+                    D = Base64UrlEncoder.Encode(parameters.D);
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"key type: {(certificate.PrivateKey ?? certificate.PublicKey.Key).GetType().Name} not supported.");
+            }
+            Alg = certificate.SignatureAlgorithm.Value switch
+            {
+                "1.2.840.113549.1.1.11" => SecurityAlgorithms.RsaSha256,
+                "1.2.840.113549.1.1.12" => SecurityAlgorithms.RsaSha384,
+                "1.2.840.113549.1.1.13" => SecurityAlgorithms.RsaSha256,
+                "1.2.840.10045.4.3.2" => SecurityAlgorithms.EcdsaSha256,
+                "1.2.840.10045.4.3.3" => SecurityAlgorithms.EcdsaSha384,
+                "1.2.840.10045.4.3.4" => SecurityAlgorithms.EcdsaSha512,
+                _ => throw new InvalidOperationException($"Unsupported algorithm of {certificate.SignatureAlgorithm.Value} - {certificate.SignatureAlgorithm.FriendlyName}"),
+            };
+        }
+
+        public IList<string> KeyOps { get; set; } = new List<string>();
+        public string Y { get; set; }
+        public string X5u { get; set; }
+        public string X5tS256 { get; set; }
+        public string X5t { get; set; }
+        public IList<string> X5c { get; set; } = new List<string>();
+        public string X { get; set; }
+        public string Use { get; set; }
+        public string QI { get; set; }
+        public string Q { get; set; }
+        public string P { get; set; }
+        public IList<string> Oth { get; set; } = new List<string>();
+        public string N { get; set; }
+        public string Kty { get; set; }
+        public string K { get; set; }
+        public string E { get; set; }
+        public string DQ { get; set; }
+        public string DP { get; set; }
+        public string D { get; set; }
+        public string Crv { get; set; }
+        public string Alg { get; set; }
+        public IDictionary<string, object> AdditionalData { get; set; } = new Dictionary<string, object>();
+        public string Kid { get; set; }
+
+
+        public bool HasPrivateKey
+        {
+            get
+            {
+                if (Kty == JsonWebAlgorithmsKeyTypes.RSA)
+                    return D != null && DP != null && DQ != null && P != null && Q != null && QI != null;
+                else if (Kty == JsonWebAlgorithmsKeyTypes.EllipticCurve)
+                    return D != null;
+                else
+                    return false;
+            }
+        }
+        public int KeySize
+        {
+            get
+            {
+                if (Kty == JsonWebAlgorithmsKeyTypes.RSA && !string.IsNullOrEmpty(N))
+                    return Base64UrlEncoder.DecodeBytes(N).Length * 8;
+                else if (Kty == JsonWebAlgorithmsKeyTypes.EllipticCurve && !string.IsNullOrEmpty(X))
+                    return Base64UrlEncoder.DecodeBytes(X).Length * 8;
+                else if (Kty == JsonWebAlgorithmsKeyTypes.Octet && !string.IsNullOrEmpty(K))
+                    return Base64UrlEncoder.DecodeBytes(K).Length * 8;
+                else
+                    return 0;
+            }
+        }
+    }
+}
---
+++ JwkGenerator/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace JwkGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Certificate files (*.cer, *.pfx)|*.cer;*.pfx";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var cert = new X509Certificate2(openFileDialog.FileName, txtPassword.Text, X509KeyStorageFlags.Exportable);
+                    var jwk = new JsonWebKeyDto(cert);
+                    txtResult.Text = JsonConvert.SerializeObject(jwk, Formatting.Indented);
+                }
+                catch (Exception exception)
+                {
+                    txtResult.Text = exception.ToString();
+                }
+            }
+        }
+    }
+
+    public static class Base64Url
+    {
+        public static string Encode(byte[] arg)
+        {
+            string s = Convert.ToBase64String(arg); // Standard base64 encoder
+
+            s = s.Split('=')[0]; // Remove any trailing '='s
+            s = s.Replace('+', '-'); // 62nd char of encoding
+            s = s.Replace('/', '_'); // 63rd char of encoding
+
+            return s;
+        }
+
+        public static byte[] Decode(string arg)
+        {
+            string s = arg;
+            s = s.Replace('-', '+'); // 62nd char of encoding
+            s = s.Replace('_', '/'); // 63rd char of encoding
+
+            switch (s.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 2: s += "=="; break; // Two pad chars
+                case 3: s += "="; break; // One pad char
+                default: throw new Exception("Illegal base64url string!");
+            }
+
+            return Convert.FromBase64String(s); // Standard base64 decoder
+        }
+    }
+}
